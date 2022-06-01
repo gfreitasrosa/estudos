2217acd4161f36204b51749e2490c13d42efb852
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2022-06-01 13:07:55 -0300

    Revert "."
    This reverts commit a7d7c9d082ae3e7d63bf031ed7ae81743c2d152e.

--- Python/Dir_e_Help.py
+++
@@ -1,9 +0,0 @@
-"""
-Utilitários Python para te auxiliar na progamação
-
-dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinando tipo de dado ou variável.
-
-dir (tipo de dado/variavel)
-help -> Apresenta a documentação/Como utilizar os atributos/propriedades e funções/métodos disponíveis
-para determinado tipo de dado ou variável.
-"""
