6463a3314c2cd6a2c05d91121eb1cfd566285562
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2022-06-08 20:38:07 -0300

    add Conjuntos.py

---
+++ Python/Conjuntos.py
@@ -0,0 +1,22 @@
+"""
+Conjuntos 
+
+- Conjuntos em qualquer linguagem de programação, estamos fazendo referência à teoria dos Conjuntos da MATEMÁTICA.
+
+- Aqui no Python, os Conjntos são chamados de Sets.
+
+Dito isto, da mesma forma que na matemática:
+
+- Sets (conjuntos) não possuem valores duplicados;
+- Sets (conjuntos) nçao possuem valores ordenados;
+- Elementos não são acessados via índice, ou seja, conjuntos não são indexados;
+
+Conjuntos sao bons para se utilizar quando precisamos armazenar elementos mas nao nos importamos com a ordenacao deles. quando nao precisamos se preocupar com chaves, valores e itens duplicados.
+
+Os conjuntos (sets) sao referenciados Python com chaves {}
+
+Diferença entre Conjuntos (Set) e Mapas (Dicionários) em Python:
+    - Um dicionário tem chave/valor;
+    - Um conjunto tem apenas valor;
+
+"""
