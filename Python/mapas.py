1dde8a799c25b61e0a103f944a825c06e7d78758
gfreitasrosa <gfreitasrosa27@gmail.com>
2022-06-08 16:41:30 -0300

    Modificação dicionários e mapas

---
+++ Python/mapas.py
@@ -0,0 +1,55 @@
+"""
+Mapas em Python são conhecidos como dicionários
+
+Identificado por chave e valor
+
+# COMO ITERAR MAPAS / DICIONÁRIOS
+
+    for chave in dicionario:
+        print(chave) -> Imprime as chaves do dicionário
+
+    for chave in dicionario:
+        print(dicionario[chave]) -> Imprime os valores do dicionário
+
+    for chave in receita:
+        print(f"{chave} : {dicionario[chave]}) -> Imprime todas as chaves e os seus valores do dicionario
+
+    for chave in receita.keys(): -> Modo pythonico
+        print(chave)
+
+    for valor in receita.values(): -> Modo pythonico
+        print(valor)
+
+# DESEMPACTAMENTO
+
+    for chave, valores in receita.items():
+        print(f"chave = {chave} valor = {valores})
+
+
+# MÉTODOS 
+
+    Soma*, Valor Máximo*, Valor mínimo*, Tamanho
+
+    * = Apenas para valores inteiros ou reais
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
