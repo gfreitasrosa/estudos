2c728715f26ff25763de2e5f3bc8de8037cdb2f0
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2022-07-05 10:50:10 -0300

    .

---
+++ Python/Funcoes.py
@@ -0,0 +1,25 @@
+"""
+Definindo funções
+
+- Funções são pequenos trechos de código que realizam tarefa específicas;
+- Pode ou não receber entradas de dados e retornar um saida de dados;
+- Muito uteis para executar procedimentos similares por repetidas vezes;
+
+OBS: Se você escreve uma função que realzia varias tarefas dentro dela, é bom fazer uma verificação
+para que a função seja simplificada
+
+# DRY -> Don´t Repeat Yourself / Não repita seu código
+
+nome_da_funcao -> SEMPRE, com letrar minusculas e se for nome composto, separado por underline (Snake Case)
+parametros_de_entrada -> Opcionais, podendo ser mais de um ou não
+bloco_da_funcao -> Pode ter retorno ou não
+
+Exemplo:
+
+    def exemplo_funcao:
+        bloco_de_codigo
+
+
+
+"""
+
