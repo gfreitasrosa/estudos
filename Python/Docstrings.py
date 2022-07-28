90056c36f1feebbbf53b1f59c82e06a1db5c6a83
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2022-07-28 08:55:47 -0300

    estudo sobre docstrings

---
+++ Python/Docstrings.py
@@ -0,0 +1,16 @@
+"""
+
+Documentando funções com Docstrings
+
+
+# Obs: Podemos ter acesso a documentação de uma função em Python utilizando a propriedade especial '__doc__'
+ou com a função 'help(nome_da_funcao)'
+
+"""
+
+# Exemplos
+
+def diz_oi():
+    """ Uma função simples que retorna a string 'Oi!'"""
+    return 'Oi!'
+
