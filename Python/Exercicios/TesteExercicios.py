56ed17ab6d72acc369b6c6cc9ee87bea2c9494d5
gfreitasrosa <gabriel-freitasrosa@hotmail.com>
2022-08-11 11:45:18 -0300

    Att README.MD

--- Python/Exercicios/TesteExercicios.py
+++ Python/Exercicios/TesteExercicios.py
@@ -1,14 +1,9 @@
-def triangulo(n):
-    altura = 2*n-1
-    largura = n
-    cont = 0
-    while cont < altura:
-        while cont < largura:
-            cont = cont + 1
-            print("*" * cont)
-        while cont > 0:
-            cont = cont - 1
-            print("*" * cont)
-        return
+string = ['[Breaks](Python/Breaks.py)', '[Dicionários](Python/Dicionarios.py)', '[Mapas](Python/Mapas.py)',
+'[Conjuntos](Python/Conjuntos.py)', '[Collections](Python/Collections.py)', '[Dir e Help](Python/Dir_e_Help.py)',
+'[Listas](Python/Listas.py)', '[Loop For](Python/Loop_for.py)', '[PEP8](Python/PEP8.py)', '[Ranges](Python/Ranges.py)',
+'[Recebendo dados do usuário](Python/Recebendo_dados_usuario.py)', '[Tuplas](Python/Tuplas.py) [Loop While](Python/While.py)]']
 
-triangulo(35)
+
+lista = sorted(string)
+
+print(lista)
